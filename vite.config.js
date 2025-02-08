19e8f62b1b6038ab351295522f3ebd51d4033134
SharconGames <sharcongaming@gmail.com>
2025-02-08 16:43:05 +0530

    Done

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import glsl from 'vite-plugin-glsl';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: 'sources',
+    publicDir: '../public',
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+    plugins: [glsl()]
+})
